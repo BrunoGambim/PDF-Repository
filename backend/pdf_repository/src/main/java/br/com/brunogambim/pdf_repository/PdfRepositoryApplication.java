7258b37395645f95057bf96aa41db22adf985639
unknown <brunor.gambim@gmail.com>
2023-04-23 14:28:44 -0300

    initing project

---
+++ backend/pdf_repository/src/main/java/br/com/brunogambim/pdf_repository/PdfRepositoryApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunogambim.pdf_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfRepositoryApplication.class, args);
+	}
+
+}
---
+++ backend/pdf_repository/src/test/java/br/com/brunogambim/pdf_repository/PdfRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunogambim.pdf_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
