f909da2c0c47f63b0dfdf514db5bd5e1fc577db0
unknown <brunor.gambim@gmail.com>
2023-05-04 08:12:39 -0300

    add-missing-exceptions-to-controller-advice

--- backend/pdf_repository/src/main/java/br/com/brunogambim/pdf_repository/api/v1/exception_handling/ExceptionHandlerController.java
+++ backend/pdf_repository/src/main/java/br/com/brunogambim/pdf_repository/api/v1/exception_handling/ExceptionHandlerController.java
@@ -2,12 +2,20 @@ package br.com.brunogambim.pdf_repository.api.v1.exception_handling;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import br.com.brunogambim.pdf_repository.core.pdf_management.exceptions.InvalidEmptyOrNullFileFieldException;
+import br.com.brunogambim.pdf_repository.core.pdf_management.exceptions.InvalidEvaluationValueException;
+import br.com.brunogambim.pdf_repository.core.pdf_management.exceptions.InvalidFileDataSizeException;
+import br.com.brunogambim.pdf_repository.core.pdf_management.exceptions.InvalidFileFormatException;
+import br.com.brunogambim.pdf_repository.core.pdf_management.exceptions.NegativeBalanceException;
 import br.com.brunogambim.pdf_repository.core.user_management.exceptions.InsufficientBalanceException;
+import br.com.brunogambim.pdf_repository.core.user_management.exceptions.InvalidEmptyOrNullUserFieldException;
 import br.com.brunogambim.pdf_repository.core.user_management.exceptions.InvalidUpdatePasswordCodeException;
 import br.com.brunogambim.pdf_repository.core.user_management.exceptions.UnauthorizedUserException;
 import br.com.brunogambim.pdf_repository.core.user_management.exceptions.UserAlreadyHasAccessToPDFException;
@@ -28,7 +36,7 @@ public class ExceptionHandlerController {
 	@ExceptionHandler(InsufficientBalanceException.class)
 	public ResponseEntity<StandartError> insufficientBalance(InsufficientBalanceException insufficientBalanceException,
 			HttpServletRequest httpServletRequest){
-		StandartError standartError = new StandartError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Insufficient Balance",
+		StandartError standartError = new StandartError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Insufficient Balance",
 				insufficientBalanceException.getMessage(), httpServletRequest.getRequestURI());
 		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(standartError);
 	}
@@ -44,8 +52,56 @@ public class ExceptionHandlerController {
 	@ExceptionHandler(UserAlreadyHasAccessToPDFException.class)
 	public ResponseEntity<StandartError> userAlreadyHasAccessToPDF(UserAlreadyHasAccessToPDFException userAlreadyHasAccessToPDFException,
 			HttpServletRequest httpServletRequest){
-		StandartError standartError = new StandartError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "User already has access to PDF",
+		StandartError standartError = new StandartError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "User already has access to PDF",
 				userAlreadyHasAccessToPDFException.getMessage(), httpServletRequest.getRequestURI());
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(standartError);
+	}
+	
+	@ExceptionHandler(InvalidEmptyOrNullUserFieldException.class)
+	public ResponseEntity<StandartError> invalidEmptyOrNullUserField(InvalidEmptyOrNullUserFieldException invalidEmptyOrNullUserFieldException,
+			HttpServletRequest httpServletRequest){
+		StandartError standartError = new StandartError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Empty field",
+				invalidEmptyOrNullUserFieldException.getMessage(), httpServletRequest.getRequestURI());
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(standartError);
+	}
+	
+	@ExceptionHandler(InvalidEmptyOrNullFileFieldException.class)
+	public ResponseEntity<StandartError> invalidEmptyOrNullUserField(InvalidEmptyOrNullFileFieldException invalidEmptyOrNullUserFieldException,
+			HttpServletRequest httpServletRequest){
+		StandartError standartError = new StandartError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Empty field",
+				invalidEmptyOrNullUserFieldException.getMessage(), httpServletRequest.getRequestURI());
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(standartError);
+	}
+	
+	@ExceptionHandler(InvalidEvaluationValueException.class)
+	public ResponseEntity<StandartError> invalidEvaluationValue(InvalidEvaluationValueException invalidEvaluationValueException,
+			HttpServletRequest httpServletRequest){
+		StandartError standartError = new StandartError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Invalid evaluation value",
+				invalidEvaluationValueException.getMessage(), httpServletRequest.getRequestURI());
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(standartError);
+	}
+	
+	@ExceptionHandler(InvalidFileDataSizeException.class)
+	public ResponseEntity<StandartError> invalidFileDataSize(InvalidFileDataSizeException invalidFileDataSizeException,
+			HttpServletRequest httpServletRequest){
+		StandartError standartError = new StandartError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Invalid file size",
+				invalidFileDataSizeException.getMessage(), httpServletRequest.getRequestURI());
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(standartError);
+	}
+	
+	@ExceptionHandler(InvalidFileFormatException.class)
+	public ResponseEntity<StandartError> invalidFileFormat(InvalidFileFormatException invalidFileFormatException,
+			HttpServletRequest httpServletRequest){
+		StandartError standartError = new StandartError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Invalid format size",
+				invalidFileFormatException.getMessage(), httpServletRequest.getRequestURI());
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(standartError);
+	}
+	
+	@ExceptionHandler(NegativeBalanceException.class)
+	public ResponseEntity<StandartError> invalidFileFormat(NegativeBalanceException negativeBalanceException,
+			HttpServletRequest httpServletRequest){
+		StandartError standartError = new StandartError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Negative Balance",
+				negativeBalanceException.getMessage(), httpServletRequest.getRequestURI());
 		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(standartError);
 	}
 	
@@ -56,11 +112,11 @@ public class ExceptionHandlerController {
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(standartError);
 	}
 	
-	/*@ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+	@ExceptionHandler(SQLIntegrityConstraintViolationException.class)
 	public ResponseEntity<StandartError> dataIntegrityViolation(SQLIntegrityConstraintViolationException dataIntegrityException,
 			HttpServletRequest httpServletRequest){
 		StandartError standartError = new StandartError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Data integrity",
 				"Data integrity violation.", httpServletRequest.getRequestURI());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standartError);
-	}*/
+	}
 }
